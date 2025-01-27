b31c211b6d7e6c44843b8da76c59bc60ccf0fef1
Rayan Barghchoun <barghchoun@wustl.edu>
2025-01-27 15:14:40 -0600

    Notes

--- src/review2/E01ReviewQuestions.java
+++ src/review2/E01ReviewQuestions.java
@@ -5,16 +5,45 @@ public class E01ReviewQuestions {
 	public static void main(String[] args) {
 		
 		//1. What is a relational operator?
-		
+		//(>, <, ==, !=, >=, <=)
+
 		//2. What is a logical operator?
-		
+		//&& (and), || (or), ! (not)
+
 		//3. What does an if statement need to do its job?
-		
-		//4. What is the difference between else and else if?
-		
+		int x = 2;
+		int y = 7;
+		if (x < y){
+			//body of if statement
+			//run if condition is true
+		} //false condition, jump down here
+
+	// //4. What is the difference between else and else if?
+	// if (condition) { 
+	// 	//do if condition if #1 is true
+	// } else if (second condition) { 
+	// 	//do if condition #2 is true
+	// } else if (third condition) { 
+	// 	//do if condition #3 is true
+	// } else {
+	// 	//do this if all condition are false
+	// }
+
 		//5. What are the different parts of a loop?
-		
+		//for (intialization; condition; update)
+
+		for (int i =0; i < 10; i++){
+			//loop body
+			System.out.println(i);
+		}
+
+		int i =0;
+		while (i < 10) {
+			i++;
+		}
+
 		//6. What's the difference between a for loop and a while loop?
 			//6a. When should you choose one over the other?
+
 	}
 }
